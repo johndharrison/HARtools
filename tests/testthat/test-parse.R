e55e3a7b1f3c26e9297d1b41ed7f817900e35b33
johndharrison <johndharrison0@gmail.com>
2016-10-26 12:55:51 -0700

    Add tests for readHAR

---
+++ tests/testthat/test-parse.R
@@ -0,0 +1,39 @@
+context("parse")
+
+test_that("nonURLFilestringGivesError", {
+  expect_error(readHAR("jj"), "har is not a list, URL or file")
+})
+
+test_that("nonHarListGivesError", {
+  expect_error(readHAR(list(2)),
+               "har does not look like a HAR list/object")
+})
+
+test_that("nonHarURLGivesError", {
+  expect_error(readHAR("http://www.google.com/ncr"),
+               "Could not parse har does not appear to be in JSON format")
+})
+
+test_that("nonHarFileGivesError", {
+  expect_error(readHAR(system.file(package = "HARtools", "DESCRIPTION")),
+               "Could not parse har does not appear to be in JSON format")
+})
+
+test_that("canParseHARUrl", {
+  expect_silent(
+    readHAR("http://www.janodvarko.cz/har/viewer/examples/google.com.har")
+  )
+})
+
+test_that("canParseHARFile", {
+  expect_silent(
+    readHAR(system.file(package = "HARtools", "exdata", "google.com.har"))
+  )
+})
+
+test_that("canParseHARList", {
+  har <- jsonlite::fromJSON(system.file(package = "HARtools",
+                              "exdata", "google.com.har"),
+                  simplifyVector = FALSE)
+  expect_silent(readHAR(har))
+})
