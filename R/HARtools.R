178e3ea375871d05bd39f68763e7b004091a8048
johndharrison <johndharrison0@gmail.com>
2016-10-26 12:51:46 -0700

    Update imports

--- R/HARtools.R
+++ R/HARtools.R
@@ -1,10 +1,11 @@
-
 #' HARtools
 #'
 #' Tools for HAR's
 #'
 #'
 #' @importFrom magrittr %>%
+#' @importFrom jsonlite fromJSON
+#' @importFrom assertthat assert_that on_failure<-
 #'
 #' @docType package
 #' @name HARtools
