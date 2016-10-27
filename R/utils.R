cab147859f47c74f666e14bf5ea4a57f6646d5b8
johndharrison <johndharrison0@gmail.com>
2016-10-27 10:35:42 -0700

    Update print methods

---
+++ R/utils.R
@@ -0,0 +1,61 @@
+#' @export
+print.har <- function(x, ...){
+  print(x[["log"]], ...)
+}
+
+#' @export
+print.harlog <- function(x, ...){
+  print(x[["version"]], ...)
+  print(x[["creator"]], ...)
+  print_if_not_null(x[["browser"]], ...)
+  print_if_not_null(x[["pages"]], ...)
+  print(x[["entries"]], ...)
+}
+
+#' @export
+print.harversion <- function(x, ...){
+  cat("--------HAR VERSION--------", "\n")
+  cat("HAR specification version:", x, "\n")
+}
+
+#' @export
+print.harcreator <- function(x, ...){
+  cat("--------HAR CREATOR--------", "\n")
+  cat("Created by:", x[["name"]], "\n")
+  cat("version:", x[["version"]], "\n")
+}
+
+#' @export
+print.harbrowser <- function(x, ...){
+  cat("--------HAR BROWSER--------", "\n")
+  cat("Browser:", x[["name"]], "\n")
+  cat("version:", x[["version"]], "\n")
+}
+
+#' @export
+print.harpages <- function(x, ...){
+  cat("--------HAR PAGES--------", "\n")
+  lapply(x, print)
+}
+
+#' @export
+print.harpage <- function(x, ...){
+  cat("Page id:", x[["id"]], ", Page title:", x[["title"]], "\n")
+}
+
+#' @export
+print.harentries <- function(x, ...){
+  cat("--------HAR ENTRIES--------", "\n")
+  cat("Number of entries:", length(x), "\n")
+}
+
+assignClass <- function(x, classx){
+  if(!inherits(x, classx)){
+    class(x) <- c(classx, class(x))
+  }
+  x
+}
+
+print_if_not_null <- function(x, ...){
+  if(!is.null(x)) print(x, ...)
+}
