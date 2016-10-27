c100f16429073d1244ea0766e02307c5e60c592f
johndharrison <johndharrison0@gmail.com>
2016-10-27 05:55:57 -0700

    Test for valid fields in har log

--- R/processHar.R
+++ R/processHar.R
@@ -1,5 +1,11 @@
 processHAR <- function(har, ...){
   harLog <- har[["log"]]
-  assert_that(contains_required_HARlog(harLog))
+  assert_that(
+    contains_required(harLog,c("version", "creator", "entries"))
+  )
+  assert_that(
+    fields_valid(harLog, c("version", "creator", "entries",
+                           "browser", "pages", "comment"))
+  )
   har
 }
--- tests/testthat/test-processHar.R
+++ tests/testthat/test-processHar.R
@@ -4,3 +4,20 @@ test_that("harLogWithoutRequiredFieldsGivesError", {
   expect_error(readHAR(list(log = list())),
                paste("version", "creator", "entries", sep = ","))
 })
+
+test_that("harLogWithinvalidFieldsGivesError", {
+  expect_error(
+    readHAR(
+      list(
+        log = list(
+          version = list(),
+          creator = list(),
+          entries = list(),
+          somefield = list()
+        )
+      )
+    ),
+    paste("version", "creator", "entries",
+          "browser", "pages", "comment", sep = ",")
+  )
+})
