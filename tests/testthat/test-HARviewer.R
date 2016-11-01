568ed40863359230994c6d3bd1ce1b4033b148bd
johndharrison <johndharrison0@gmail.com>
2016-11-01 08:31:59 -0700

    Add tests for HARviewer

---
+++ tests/testthat/test-HARviewer.R
@@ -0,0 +1,19 @@
+context("HARviewer")
+
+test_that("canCreateaHARviewer", {
+  har <- readHAR(system.file(package = "HARtools", "exdata",
+                             "r-project.org.161028_W2_11MA.har"))
+  hv <- HARviewer(har)
+  expect_s3_class(hv, "HARviewer")
+})
+
+test_that("canCreateHARviewerOutput", {
+  hvOutput <- HARviewerOutput("myHAR")
+  expect_s3_class(hvOutput, "shiny.tag.list")
+})
+
+test_that("canRenderHARviewer", {
+  hv <- HARviewer(har)
+  rHV <- renderHARviewer(hv)
+  expect_s3_class(rHV, "shiny.render.function")
+})
