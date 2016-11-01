deac4b112a76ff9a7629caf560c8d4b3a661aa75
johndharrison <johndharrison0@gmail.com>
2016-11-01 08:37:33 -0700

    Fix typo in test

--- tests/testthat/test-HARviewer.R
+++ tests/testthat/test-HARviewer.R
@@ -13,6 +13,8 @@ test_that("canCreateHARviewerOutput", {
 })
 
 test_that("canRenderHARviewer", {
+  har <- readHAR(system.file(package = "HARtools", "exdata",
+                             "r-project.org.161028_W2_11MA.har"))
   hv <- HARviewer(har)
   rHV <- renderHARviewer(hv)
   expect_s3_class(rHV, "shiny.render.function")
